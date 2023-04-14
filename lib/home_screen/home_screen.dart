b382bbdb3e5379608003e9b78b30b39b38d35b8b
Alex Duarte <alexduarteduartesilva@gmail.com>
2023-04-14 16:17:52 -0300

    Creating search box

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/widgets/search_box.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,21 +7,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFEEEFF5),
-        leading: const Icon(Icons.menu),
-        actions: const [
-           Padding(
-            padding: EdgeInsets.only(right: 10.0),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/avatar.jpeg'),
-            ),
-          )
-        ],
-        iconTheme: const IconThemeData(
-          color: Colors.black,
+      backgroundColor: const Color(0xFFEEEFF5),
+      appBar: _buildAppBar(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
         ),
       ),
     );
   }
 }
+
+AppBar _buildAppBar() {
+  return AppBar(
+    elevation: 0,
+    backgroundColor: const Color(0xFFEEEFF5),
+    leading: const Icon(Icons.menu),
+    actions: const [
+      Padding(
+        padding: EdgeInsets.only(right: 15.0),
+        child: CircleAvatar(
+          backgroundImage: AssetImage('assets/images/avatar.jpeg'),
+        ),
+      )
+    ],
+    iconTheme: const IconThemeData(
+      color: Colors.black,
+    ),
+  );
+}
+
+
